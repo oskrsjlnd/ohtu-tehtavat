2812f3c61bfb531d89280c22509d36ecab6e29bd
Oskar Sjölund <oskar.sjolund93@gmail.com>
2021-11-08 23:52:07 +0200

    tests updated

--- viikko1/nhl-statistics-1/src/tests/statistics_test.py
+++ viikko1/nhl-statistics-1/src/tests/statistics_test.py
@@ -17,25 +17,16 @@ class TestStatistics(unittest.TestCase):
         self.statistics = Statistics(PlayerReaderStub())
 
     def test_top_scorers_found(self):
-        sorted = self.statistics.top_scorers(2)
-        tops = [
-            Player("Gretzky", "EDM", 35, 89),
-            Player("Lemieux", "PIT", 45, 54),
-            Player("Yzerman", "DET", 42, 56)
-        ]
-        self.assertIsInstance(sorted, list)
-        self.assertEqual(sorted[0].name, tops[0].name)
-        self.assertEqual(sorted[2].name, tops[2].name)
+        found = self.statistics.top_scorers(2)
+        self.assertListEqual(found, [Player("Gretzky", "EDM", 35, 89), Player("Lemieux", "PIT", 45, 54), Player("Yzerman", "DET", 42, 56)])
         
     
     def test_search_by_wrong_team_name_returns_emptylist(self):
-        self.assertEqual(self.statistics.team("Keijo"), list())
+        found = self.statistics.team("Keijo")
+        self.assertListEqual(found, [])
 
     def test_search_by_team_returns_all_team_players(self):
-        self.assertEqual(self.statistics.team("EDM"), [\
-            Player("Semenko", "EDM", 4, 12),\
-            Player("Kurri", "EDM", 37, 53),\
-            Player("Gretzky", "EDM", 35, 89)])
+        self.assertListEqual(self.statistics.team("EDM"), [Player("Semenko", "EDM", 4, 12), Player("Kurri", "EDM", 37, 53), Player("Gretzky", "EDM", 35, 89)])
 
     def test_search_finds_correct_player(self):
         self.assertEqual(self.statistics.search("Semenko").name,"Semenko" )
