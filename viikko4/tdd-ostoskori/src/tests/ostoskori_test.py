657f1523e497fc3cf79eab32c4b7b38701576549
Oskar Sjölund <oskar.sjolund93@gmail.com>
2021-11-29 21:29:49 +0200

    after adding 2 items, price is same as twice the item price

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -6,6 +6,8 @@ class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
         self.maito = Tuote("Maito", 3)
+        self.leipa = Tuote("Leipä", 2)
+
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
@@ -21,21 +23,25 @@ class TestOstoskori(unittest.TestCase):
         self.assertEqual(self.kori.hinta(), 3)
     
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorissa_kaksi_tavaraa(self):
-        leipa = Tuote("Leipä", 2)
         self.kori.lisaa_tuote(self.maito)
-        self.kori.lisaa_tuote(leipa)
+        self.kori.lisaa_tuote(self.leipa)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
     
     def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_sama_kuin_tuotteiden_hintojen_summa(self):
-        leipa = Tuote("Leipä", 2)
         self.kori.lisaa_tuote(self.maito)
-        self.kori.lisaa_tuote(leipa)
+        self.kori.lisaa_tuote(self.leipa)
 
         self.assertEqual(self.kori.hinta(), 5)
     
-    def test_kahden_saman_tuotteen_lisäämisen_jälkeen_korissa_kaksi_tavaraa(self):
+    def test_kahden_saman_tuotteen_lisaamisen_jälkeen_korissa_kaksi_tavaraa(self):
         self.kori.lisaa_tuote(self.maito)
         self.kori.lisaa_tuote(self.maito)
 
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+    
+    def test_kahden_saman_tuotteen_lisayksen_jalkeen_hinta_kahden_tuotteen_hinta(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        
+        self.assertEqual(self.kori.hinta(), 6)
